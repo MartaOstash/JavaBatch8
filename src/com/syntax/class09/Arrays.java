3bc353fb5a18146412e58497d3a7554a3c4dadc4
MartaOstash <martaostash.18@gmail.com>
2020-10-24 11:01:58 -0400

    HOmework REcap

--- src/com/syntax/class09/Arrays.java
+++ src/com/syntax/class09/Arrays.java
@@ -19,7 +19,7 @@ public class Arrays {
 		num[3] = 40;
 		num[4] = 50;
 
-		// to access element from an array?
+		// how to access element from an array?
 
 		System.out.println("Accessing 3 element " + num[2]);
 		System.out.println(num[1] + num[4]);
@@ -29,6 +29,17 @@ public class Arrays {
 		num[4] = 100;
 		System.out.println("Value of 5th element after reassigning " + num[4]);
 
-	}
+		// int[] array=new int[2];
+		// array[0]=10.99;// compiler error cause of 2 different types;
 
+		String[] array = new String[4];
+
+		array[0] = "Hello";
+		array[1] = "HI";
+		array[2] = "Bye";
+		array[3] = "Good morning";
+		array[4] = "Good evening";
+		System.out.println(array[3]);
+
+	}
 }
---
+++ src/com/syntax/reviewClass04/Equals.java
@@ -0,0 +1,12 @@
+package com.syntax.reviewClass04;
+
+public class Equals {
+
+	public static void main(String[] args) {
+		String name = null;
+		if ("Marta".equals(name)) {
+			System.out.println("Hi, Marta!");
+		}
+	}
+
+}
