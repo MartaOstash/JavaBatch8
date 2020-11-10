9995c9343ba205aa1be01fe726f5efb3a4006cf1
MartaOstash <martaostash.18@gmail.com>
2020-11-10 09:36:22 -0500

    Class 14 -15

---
+++ src/com/CalebCurry/Task1.java
@@ -0,0 +1,16 @@
+package com.CalebCurry;
+
+public class Task1 {
+
+	public String email;
+
+	public void setEmail(String email1) {
+		email = email1;
+
+	}
+
+	public String email() {
+		return email;
+	}
+
+}
